7a596be0c6afbd6718e4c7ac638c92080f8ec44e
seanpmaxwell <spmaxwell7@gmail.com>
2022-02-21 12:04:33 -0800

    removed auth dal

--- server/src/dal/user.go
+++ server/src/dal/user.go
@@ -69,3 +69,27 @@ func DeleteUser(id uint) error {
 	}
 	return nil
 }
+
+/**
+Fetch a user's hashed password
+*/
+func GetPwdHash(userId uint) (string, error) {
+	var userCreds models.UserCreds
+	resp := db.Where("user_id = ?", userId).First(&userCreds)
+	if resp.Error != nil {
+		return "", resp.Error
+	}
+	return userCreds.Pwdhash, nil
+}
+
+/**
+Create a user credentials row to store confidentials stuff.
+*/
+func SaveUserCreds(id uint, pwdHash string) error {
+	userCreds := models.UserCreds{Pwdhash: pwdHash, UserID: id}
+	resp := db.Save(&userCreds)
+	if resp.Error != nil {
+		return resp.Error
+	}
+	return nil
+}
